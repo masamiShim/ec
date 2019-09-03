ae5cb93a16df7ad6c61b0abdc8394c186d4c19f3
shinbuichi <shinbuichi@iij.ad.jp>
2019-09-03 13:22:12 +0900

    initial commit

---
+++ src/main/kotlin/freitech/se/ec/EcApplication.kt
@@ -0,0 +1,11 @@
+package freitech.se.ec
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcApplication>(*args)
+}
