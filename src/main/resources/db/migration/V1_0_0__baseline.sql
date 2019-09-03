7ce6c73c8871c2314a6b5f1cbb0b72a53513051b
shinbuichi <shinbuichi@iij.ad.jp>
2019-09-03 19:00:57 +0900

    テーブル追加途中

---
+++ src/main/resources/db/migration/V1_0_0__baseline.sql
@@ -0,0 +1,79 @@
+/*
+create database ec_db;
+create user if not exists 'ec_user'@'%' IDENTIFIED BY 'ec_user_01';
+grant all ON ec_db.* TO ec_user@'%';
+FLUSH PRIVILEGES ;
+*/
+
+create table if not exists ec_db.Exhibitor (
+  id int auto_increment,
+  name varchar(255) not null,
+  email varchar(255) not null,
+  password varchar(255) not null,
+  verified bit not null,
+  created timestamp not null,
+  createdBy int not null,
+  modified timestamp not null,
+  modifiedBy int not null,
+  primary key (id)
+)
+
+create table if not exists ec_db.Customer (
+  id int auto_increment,
+  name varchar(255) not null,
+  email varchar(255) not null,
+  password varchar(255) not null,
+  verified bit not null,
+  created timestamp not null,
+  createdBy int not null,
+  modified timestamp not null,
+  modifiedBy int not null,
+  primary key (id)
+)
+
+create table if not exists ec_db.Item (
+  id int auto_increment,
+  exhibitor_id int not null,
+  name varchar(255) not null,
+  code varchar(255) not null,
+  price varchar(255) not null,
+  quantity int not null,
+  created timestamp not null,
+  createdBy int not null,
+  modified timestamp not null,
+  modifiedBy int not null,
+  primary key (id)
+)
+
+create table if not exists ec_db.Item_Image (
+  id int auto_increment,
+  item_id int not null,
+  path varchar(255) not null,
+  created timestamp not null,
+  createdBy int not null,
+  modified timestamp not null,
+  modifiedBy int not null,
+  is_primary bit not null,
+  primary key (id)
+)
+
+create table if not exists ec_db.Chat_Room (
+  id int auto_increment,
+  item_id int not null,
+  customer_id int not null,
+  created timestamp not null,
+  createdBy int not null,
+  modified timestamp not null,
+  modifiedBy int not null,
+  primary key (id)
+)
+
+create table if not exists ec_db.Chat (
+  id int auto_increment,
+  chat_room_id int not null,
+  side bit not null,
+  comment varchar(511) not null,
+  created timestamp not null,
+  createdBy int not null,
+  primary key (id)
+)
