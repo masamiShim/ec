4b2bdd09e13bd435a6e91922d4aa2621e916098b
shinbuichi <shinbuichi@iij.ad.jp>
2019-09-06 17:09:09 +0900

    mybatis用のリポジトリ追加途中

---
+++ src/main/kotlin/freitech/se/ec/repository/read/ExhibitorReadRepository.kt
@@ -0,0 +1,9 @@
+package freitech.se.ec.repository.read
+
+import org.apache.ibatis.annotations.Mapper
+import org.springframework.stereotype.Repository
+
+@Repository
+@Mapper
+class ExhibitorReadRepository {
+}
---
+++ src/main/kotlin/freitech/se/ec/repository/write/ExhibitorWriteRepository.kt
@@ -0,0 +1,9 @@
+package freitech.se.ec.repository.write
+
+import org.apache.ibatis.annotations.Mapper
+import org.springframework.stereotype.Repository
+
+@Repository
+@Mapper
+class ExhibitorWriteRepository {
+}
